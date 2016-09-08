b2e06902d5888db25151729ce3174387e5628d69
Pepejovi <pepejovi@gmail.com>
2016-09-08 13:36:34 +0300

    Toimiva timedelay jne

--- Carkit_code/Carkit_code/Carkit_code.ino
+++ Carkit_code/Carkit_code/Carkit_code.ino
@@ -13,7 +13,7 @@ int speedPinB = 10;
 int speed;
 int dir;
 int RECV_PIN = 11;
-long unsigned int IRcode;
+long unsigned int IRcode, gap, lastPress, timeoutDelay;
 
 IRrecv irrecv(RECV_PIN);
 
@@ -41,136 +41,91 @@ void setup(){
   speed = 255;
   dir = 4;
   irrecv.enableIRIn(); // Start the receiver
+  timeoutDelay = 100;
 }
 
 void loop() {
   if (irrecv.decode(&results)) 
   {
-/*    if(results.value == OK.value)
-    {
-      speed = 0;
-    }*/
+    lastPress = millis();
+    if(results.value == UP.value){old_value.value = results.value; dir = 1;}
+    if(results.value == DOWN.value){old_value.value = results.value; dir = 0;}
+    if(results.value == LEFT.value){old_value.value = results.value; dir = 3;}
+    if(results.value == RIGHT.value){old_value.value = results.value; dir = 2;}
     
-    if(results.value == UP.value){old_value.value = UP.value;}   
-    if(results.value == DOWN.value){old_value.value = DOWN.value;}
-    if(results.value == LEFT.value) {old_value.value = LEFT.value;}
-    if(results.value == RIGHT.value) {old_value.value = RIGHT.value;}
-       
     if(results.value == CONTINUOUS.value)
     { 
       Serial.println("CONTINUOUS");
       if(old_value.value == UP.value)
       {
-        Serial.print("   UP");
         dir = 1;
         speed = 255;
       }
       else if(old_value.value == DOWN.value)
       {
-        Serial.print("   DOWN");
         dir = 0;
         speed = 255;
       }
       else if(old_value.value == LEFT.value)
       {
-        Serial.print("   LEFT");
         dir = 3;
         speed = 255;
       }  
       else if(old_value.value == RIGHT.value)
       {
-        Serial.print("   RIGHT");
         dir = 2;
         speed = 255;
-      }    
-      else{
-       Serial.println("  NA"); 
       }      
     }
-    else{
-    speed = 0;
+    irrecv.resume(); // Receive the next value
+  }
+  /*Serial.print("Millis:  ");
+  Serial.println(millis());
+  Serial.print("LastPress:  ");
+  Serial.println(lastPress);*/
+  if((millis() - lastPress) >= timeoutDelay)
+  {
+    Stop();
+  }
+  
+    analogWrite(speedPinA, speed);
+    analogWrite(speedPinB, speed);
+    
+    if(dir == 0)
+    {
+    digitalWrite(dir1PinA, HIGH); // Kääntää taaksepäin
+    digitalWrite(dir2PinA, LOW);
+    digitalWrite(dir1PinB, LOW);
+    digitalWrite(dir2PinB, HIGH);
     }
-    
-        
-    analogWrite(speedPinA,255 - speed);
-    analogWrite(speedPinB,255 - speed);
-    
-    if(dir == 1)
+    else if(dir == 1)
     {
     digitalWrite(dir1PinA, LOW); //Kääntää eteenpäin
     digitalWrite(dir2PinA, HIGH); 
     digitalWrite(dir1PinB, HIGH);
     digitalWrite(dir2PinB, LOW);
-    irrecv.resume();
     }
-    else if(dir == 0)
-    {
-    digitalWrite(dir1PinA, HIGH); // Kääntää taaksepäin
-    digitalWrite(dir2PinA, LOW);
-    digitalWrite(dir1PinB, LOW);
-    digitalWrite(dir2PinB, HIGH);
-    irrecv.resume();
-    }
-    else if(dir == 3)
+    else if(dir == 2)
     {
     digitalWrite(dir1PinA, HIGH); // Kääntää vasempaan
     digitalWrite(dir2PinA, LOW);
     digitalWrite(dir1PinB, HIGH);
     digitalWrite(dir2PinB, LOW);
-    irrecv.resume();
     }
-    else if(dir == 2)
+    else if(dir == 3)
     {
     digitalWrite(dir1PinA, LOW); // Kääntää oikeaan
     digitalWrite(dir2PinA, HIGH);
     digitalWrite(dir1PinB, LOW);
     digitalWrite(dir2PinB, HIGH);
-    irrecv.resume();
     }
-    else if(dir == 4){
-     speed = 0;
-     irrecv.resume();
+    else
+    {
+      Stop();
     }
-    
-    irrecv.resume(); // Receive the next value
-  }
-
-  
- 
-//    if (irrecv.decode(&results)) {
-//    Serial.println(results.value, HEX);
-//    irrecv.resume(); // Receive the next value
-//  }
 }
-
-
-
-
-
-
- /*analogWrite(speedPinA,speed); //Oikean puolen renkaat
-  analogWrite(speedPinB,255 - speed); //Vasemman puolen renkaat
-  // set direction
-  if(1 == dir) {
-    digitalWrite(dir1PinA, LOW);
-    digitalWrite(dir2PinA, HIGH); //Kääntää eteenpäin
-    digitalWrite(dir1PinB, HIGH);
-    digitalWrite(dir2PinB, LOW);
-  }else{
-    digitalWrite(dir1PinA, HIGH); // Kääntää taaksepäin
-    digitalWrite(dir2PinA, LOW);
-    digitalWrite(dir1PinB, LOW);
-    digitalWrite(dir2PinB, HIGH);
-  }
-  if(millis()-time>5000) {
-    time = millis();
-    speed += 20;
-    if(speed > 255){
-      speed = 0;
-    }
-    if(1==dir){
-      dir = 0;
-    }else{
-      dir = 1;
-    }
-  }*/
+void Stop()
+{
+  speed = 0;
+  dir = 4;
+}
