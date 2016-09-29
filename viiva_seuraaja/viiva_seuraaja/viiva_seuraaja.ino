4d64193bfaf5185894ffb59a3627bc8021dee465
smellmee <smellmee@hotmail.com>
2016-09-29 13:05:44 +0300

    ebin viivan seuraaja

--- viiva_seuraaja/viiva_seuraaja/viiva_seuraaja.ino
+++ viiva_seuraaja/viiva_seuraaja/viiva_seuraaja.ino
@@ -1,26 +1,34 @@
 ///Arduino Sample Code
 
 #include <IRremote.h>
+
 //motor A
 int dir1PinA = 2;
 int dir2PinA = 3;
-int speedPinA = 9;
+//int speedPinA = A0;
 //motor B
 int dir1PinB = 4;
 int dir2PinB = 5;
-int speedPinB = 10;
+//int speedPinB = A1;
+int generalSpeedPin = 10;
 //Nopeus & Suunta
 int speed;
 int dir;
 //Viivan seuraaja
-int lineReaderLeftPin = 1;
-int lineReaderMiddlePin = 7;
-int lineReaderRightPin = 8;
+int lineReaderLeftPin = 7;
+int lineReaderMiddlePin = 8;
+int lineReaderRightPin = 9;
 
 short int leftSensorValue = 0;
 short int middleSensorValue = 0;
 short int rightSensorValue = 0;
 
+short int leftSensorPreviousValue = 0;
+short int middleSensorPreviousValue = 0;
+short int rightSensorPreviousValue = 0;
+
+short int lineToFollow = 1; //0 on valkoinen 1 on musta
+short int delayTime =
 //Kaukosäädin
 int RECV_PIN = 11;
 long unsigned int IRcode, gap, lastPress, timeoutDelay;
@@ -45,11 +53,11 @@ void setup()
   STOP.value = 0;
   pinMode(dir1PinA, OUTPUT);
   pinMode(dir2PinA, OUTPUT);
-  pinMode(speedPinA, OUTPUT);
+  //pinMode(speedPinA, OUTPUT);
   pinMode(dir1PinB, OUTPUT);
   pinMode(dir2PinB, OUTPUT);
-  pinMode(speedPinB, OUTPUT);
-  speed = 25;
+  pinMode(generalSpeedPin, OUTPUT);
+  speed = 140;
   dir = 4;
   irrecv.enableIRIn(); // Start the receiver
   timeoutDelay = 100; 
@@ -59,45 +67,52 @@ void loop()
    if (irrecv.decode(&results)) 
   {
     if(results.value == STAR.value){
-        if(followLine){
+        if(followLine == true){
           followLine = false;
       }else{
        followLine = true; 
       }
      }
   }
+  
   if(followLine){
     leftSensorValue = digitalRead(lineReaderLeftPin); // Arvo on 0 kun valkoista ja 1 jos mustaa
     middleSensorValue = digitalRead(lineReaderMiddlePin);
     rightSensorValue = digitalRead(lineReaderRightPin);
     
-    
-    Serial.print("Left: ");
+    /*if(leftSensorValue != leftSensorPreviousValue|| middleSensorValue != middleSensorPreviousValue|| rightSensorValue != rightSensorPreviousValue)
+    { }*/
+    /*Serial.print("Left: ");
     Serial.print(leftSensorValue); 
      Serial.print("  Mid: ");
     Serial.print(middleSensorValue);
      Serial.print("  right: ");
-    Serial.println(rightSensorValue);
+    Serial.println(rightSensorValue);*/
     
-    if(leftSensorValue == 0 && rightSensorValue != 0)
+    
+    //Käännä vaan toista takapuolta
+    if(rightSensorValue == lineToFollow && leftSensorValue != lineToFollow && middleSensorValue != lineToFollow)
     {
      //Kääntyy vasempaan 
      digitalWrite(dir1PinA, HIGH); // Kääntää vasempaan
      digitalWrite(dir2PinA, LOW);
      digitalWrite(dir1PinB, HIGH);
      digitalWrite(dir2PinB, LOW);
-    }else if(rightSensorValue == 0 && leftSensorValue != 0){
+     speed = 140;
+    }else if(leftSensorValue == lineToFollow && rightSensorValue != lineToFollow && middleSensorValue != lineToFollow){
       //Kääntyy oikealle
       digitalWrite(dir1PinA, LOW); // Kääntää oikeaan
       digitalWrite(dir2PinA, HIGH);
       digitalWrite(dir1PinB, LOW);
       digitalWrite(dir2PinB, HIGH);
-    }else if(middleSensorValue != 0 && rightSensorValue != 0 && leftSensorValue != 0){
+      speed = 140;
+    }else if(middleSensorValue != lineToFollow && rightSensorValue != lineToFollow && leftSensorValue != lineToFollow){
      //Sensori on kadottanut valkoisen viivan kokonaan. Ajaa taaksepäin? 
      digitalWrite(dir1PinA, HIGH); // Kääntää taaksepäin
      digitalWrite(dir2PinA, LOW);
      digitalWrite(dir1PinB, LOW);
      digitalWrite(dir2PinB, HIGH);
+     speed = 85;
     }else{
     //Joko kaikki sensorit näkee viivan tai pelkästään keskimmäinen sensori näkee viivan
     //Auto ajaa siis eteenpäin 
@@ -105,7 +120,15 @@ void loop()
     digitalWrite(dir2PinA, HIGH); 
     digitalWrite(dir1PinB, HIGH);
     digitalWrite(dir2PinB, LOW);
+    speed = 85;
     }
-  }
-  
+    analogWrite(generalSpeedPin, speed);
+    delay(50);
+    //analogWrite(speedPinB, speed);
+  }/*else{
+    analogWrite(generalSpeedPin, 0);
+  }*/
+  leftSensorPreviousValue = leftSensorValue;
+  middleSensorPreviousValue = middleSensorValue;
+  rightSensorPreviousValue = rightSensorValue;
 }
